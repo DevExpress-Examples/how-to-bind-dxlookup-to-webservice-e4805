0cb9db472c93677b6584c72905ac296748b6a03c
DevExpressExampleBot <fakeEmail>
2020-12-08 19:40:18 +0300

    Source auto update [skip ci]

---
+++ VB/JS/dxSample_Mobile/DxSample/views/index.js
@@ -0,0 +1,23 @@
+﻿DxSample.Index = function (params) {
+    var viewModel = {
+
+        dSource: {
+            changed: new $.Callbacks(),
+            load: function (loadOptions) {
+                var dfd = new $.Deferred();
+                if (loadOptions.refresh) {
+                    $
+                    .get('http://sampleservices.devexpress.com/api/Categories')
+                    .done(function (result) {
+                        dfd.resolve(result);
+                    });
+                    return dfd.promise();
+                }
+            },
+            byKey: function (key) {
+                return $.get('http://sampleservices.devexpress.com/api/Categories/' + key.toString());
+            }
+        }
+    };
+    return viewModel;
+};
